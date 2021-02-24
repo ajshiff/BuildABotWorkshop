bdb0d237e8d8363515b7b509810ba80f807f856e
vAdyar <vikas.adyar@intertrust.com>
2021-02-24 10:32:39 +0530

    Create Speed Value Object
    Adding the domain object as per the UML diagram on the issue.

---
+++ domain/src/main/java/com/jakefoundation/buildabotworkshop/domain/Speed.java
@@ -0,0 +1,14 @@
+package com.jakefoundation.buildabotworkshop.domain;
+
+public class Speed {
+
+    private final double value;
+
+    public Speed(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
