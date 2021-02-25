7c302b6f041b90521c305466412075ad9a5d73c8
Aaron Shiffler <ajshiffler@gmail.com>
2021-02-24 18:31:24 -0700

    added final modifier to value

--- domain/src/main/java/com/jakefoundation/buildabotworkshop/domain/HitPoints.java
+++ domain/src/main/java/com/jakefoundation/buildabotworkshop/domain/HitPoints.java
@@ -1,3 +1,4 @@
+package com.jakefoundation.buildabotworkshop.domain;
 /* Create a class in Domain called HitPoints.
 
 Give it a single constructor that requires an integer representing the number of HitPoints there are.
@@ -6,15 +7,11 @@ Let its value field be private final. (Final makes it not writable outside of th
 
 Provide a getter for that value.*/
 
-
 public class HitPoints {
     //class in domain
 
-    private int value = 5;
+    private final int value;
     //private  - number of hit points
-
-    public HitPoints() {
-    }
 
     public HitPoints(int theValue) {
         //constructor
