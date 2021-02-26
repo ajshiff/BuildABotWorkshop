eea2ebd03e2267c12804b9ea593faea17fcdffff
Aaron Shiffler <ajshiffler@gmail.com>
2021-02-26 10:35:53 -0700

    removed template comment

--- domain/src/test/java/com/jakefoundation/buildabotworkshop/domain/AngleTests.java
+++ domain/src/test/java/com/jakefoundation/buildabotworkshop/domain/AngleTests.java
@@ -3,13 +3,6 @@ package com.jakefoundation.buildabotworkshop.domain;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class AngleTests {
     @Test
     public void negativeAngle_givesPositiveAngle () {
