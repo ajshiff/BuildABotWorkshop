17fc39731b83ba27292082f5d6156e869a9c3a8c
Michael Cabrera <mjcabrera@gmail.com>
2021-02-25 22:10:13 -0700

    adding bot.java

---
+++ domain/src/main/java/com/jakefoundation/buildabotworkshop/domain/Bot.java
@@ -0,0 +1,13 @@
+package com.jakefoundation.buildabotworkshop.domain;
+
+public interface Bot {
+    {
+        public void moveCommand(GameState);
+        public void fireCommand(GameState);
+        public void String getBotCommonName;
+        public void String getOwnerName;
+        public void String getOwnerPassword;
+
+
+    }
+}
