858aefb4c87917b1a5fa50923766e5f512f0d11b
Aaron Shiffler <ajshiffler@gmail.com>
2021-02-26 10:33:39 -0700

    adjusted white-space for style

--- domain/src/main/java/com/jakefoundation/buildabotworkshop/domain/Owner.java
+++ domain/src/main/java/com/jakefoundation/buildabotworkshop/domain/Owner.java
@@ -1,22 +1,18 @@
 package com.jakefoundation.buildabotworkshop.domain;
-/* Create a class named Owner in Domain and implement it according to this UML.
- * Let private fields be final.
- * */
 
 public class Owner {
 
     private final String ownerName;
     private final int ownerId;
 
-public Owner(String theName , int theId) {
-    this.ownerName = theName;
-    this.ownerId = theId;
-}
+    public Owner(String theName , int theId) {
+        this.ownerName = theName;
+        this.ownerId = theId;
+    }
 
     public String getOwnerName() {
         return ownerName;
     }
-
     public int getOwnerId() {
         return ownerId;
     }
