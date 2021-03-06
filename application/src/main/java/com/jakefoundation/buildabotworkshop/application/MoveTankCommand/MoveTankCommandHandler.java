d189576c5e254593d15924c63d619a7cedc3dc01
Michael Cabrera <mjcabrera@gmail.com>
2021-03-06 16:22:08 -0700

    MoveTankCommandHandler

---
+++ application/src/main/java/com/jakefoundation/buildabotworkshop/application/MoveTankCommand/MoveTankCommandHandler.java
@@ -0,0 +1,31 @@
+package com.jakefoundation.buildabotworkshop.application.MoveTankCommand;
+
+import com.jakefoundation.buildabotworkshop.application.common.Authorization;
+import com.jakefoundation.buildabotworkshop.application.common.ResultStatus;
+import com.jakefoundation.buildabotworkshop.domain.MoveTankCommand;
+import com.jakefoundation.buildabotworkshop.domain.ShouldDo;
+
+public class MoveTankCommandHandler {
+
+
+    private final IMoveTankCommandService moveTankCommandService;
+
+    public MoveTankCommandHandler(IMoveTankCommandService moveTankCommandService) {
+        this.moveTankCommandService = moveTankCommandService;
+
+    }
+
+    public ResultStatus handleMoveTankCommand(MoveTankCommand moveTankCommand, Authorization authorization) {
+        if (moveTankCommand.getShouldMove() == ShouldDo.dont) {
+            return ResultStatus.success;
+
+        } else {
+            MoveTankRequest mike = new MoveTankRequest(authorization, moveTankCommand);
+            return moveTankCommandService.sendMoveTankRequest(mike);
+
+        }
+
+    }
+}
+
+
