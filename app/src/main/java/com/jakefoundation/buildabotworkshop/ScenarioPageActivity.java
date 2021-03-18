c1a933615d2b28dce7653ae355e8dbeb40909010
pvfpinho <pvfpinho@hotmail.com>
2021-03-17 21:24:12 -0600

    Scenario card - by pamRodrigues

--- app/src/main/java/com/jakefoundation/buildabotworkshop/ScenarioPageActivity.java
+++ app/src/main/java/com/jakefoundation/buildabotworkshop/ScenarioPageActivity.java
@@ -43,57 +43,65 @@ public class ScenarioPageActivity extends AppCompatActivity implements View.OnCl
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemReselectedListener((BottomNavigationView.OnNavigationItemReselectedListener) navListener);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new scenario_Fragment()).commit();
     }
 
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.fire:
-                Toast.makeText(this, "Fire tutorial", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.move:
-                Toast.makeText(this, "Move tutorial", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.scenario_random:
-                Toast.makeText(this, "Random Scenario", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.scenario_bot:
-                Toast.makeText(this, "You x Bot", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.scenario_x:
-                Toast.makeText(this, "Scenario x", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.scenario_y:
-                Toast.makeText(this, "Scenario y", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.scenario_ffa:
-                Toast.makeText(this, "Scenario FFA", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
 
-    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
-        Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case bot_launcher:
+                            selectedFragment = new bot_Fragment();
+                            break;
+                        case play_launcher:
+                            selectedFragment = new play_Fragment();
+                            break;
+                        case scenario_launcher:
+                            selectedFragment = new scenario_Fragment();
+                            break;
 
-        switch (item.getItemId()){
-            case bot_launcher:
-                selectedFragment = new bot_Fragment();
-                break;
-            case play_launcher:
-                selectedFragment = new play_Fragment();
-                break;
-            case scenario_launcher:
-                selectedFragment = new scenario_Fragment();
-                break;
+                    }
 
-        }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment);
+                    return true;
+                }
+            };
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment);
-        return true;
-    };
 
-}
+                @Override
+                public void onClick(View v) {
+                    switch (v.getId()) {
+                        case R.id.fire:
+                            Toast.makeText(this, "Fire tutorial", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.move:
+                            Toast.makeText(this, "Move tutorial", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.scenario_random:
+                            Toast.makeText(this, "Random Scenario", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.scenario_bot:
+                            Toast.makeText(this, "You x Bot", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.scenario_x:
+                            Toast.makeText(this, "Scenario x", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.scenario_y:
+                            Toast.makeText(this, "Scenario y", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.scenario_ffa:
+                            Toast.makeText(this, "Scenario FFA", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
 
+            }
 
 
