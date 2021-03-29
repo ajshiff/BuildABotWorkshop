da8cd7e42628705ade7cfd7c5b895fd22ea84875
Aaron Shiffler <ajshiffler@gmail.com>
2021-03-29 14:24:44 -0600

    added upward navigation

--- app/src/main/java/com/jakefoundation/buildabotworkshop/MainActivity.java
+++ app/src/main/java/com/jakefoundation/buildabotworkshop/MainActivity.java
@@ -1,12 +1,16 @@
 package com.jakefoundation.buildabotworkshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavController navController;
 
     public MainActivity() {
         super(R.layout.activity_main);
@@ -15,7 +19,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        navController.navigateUp();
+        return super.onSupportNavigateUp();
+    }
+
     public void inspectBots (View view) {
         Navigation.findNavController(view).navigate(R.id.action_mainMenu_to_botInspector);
     }
