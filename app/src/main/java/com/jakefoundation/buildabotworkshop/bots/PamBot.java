c0a68887dbf9f4261d932b40067f092a2c220560
pvfpinho <pvfpinho@hotmail.com>
2021-04-03 18:50:09 -0600

    Scenario card - by pamRodrigues

--- app/src/main/java/com/jakefoundation/buildabotworkshop/bots/PamBot.java
+++ app/src/main/java/com/jakefoundation/buildabotworkshop/bots/PamBot.java
@@ -1,13 +1,18 @@
 package com.jakefoundation.buildabotworkshop.bots;
 
+
+import com.jakefoundation.buildabotworkshop.domain.Angle;
 import com.jakefoundation.buildabotworkshop.domain.Bot;
 import com.jakefoundation.buildabotworkshop.domain.FireBulletCommand;
 import com.jakefoundation.buildabotworkshop.domain.GameState;
 import com.jakefoundation.buildabotworkshop.domain.MoveTankCommand;
+import com.jakefoundation.buildabotworkshop.domain.ShouldDo;
+import com.jakefoundation.buildabotworkshop.domain.Speed;
+import java.util.Objects;
 
-import kotlin.NotImplementedError;
 
 public class PamBot implements Bot {
+
 
     @Override
     public String getBotCommonName() {
@@ -16,12 +21,21 @@ public class PamBot implements Bot {
 
     @Override
     public MoveTankCommand moveCommand(GameState gameState) {
-        throw new NotImplementedError();
-    }
+
+
+        final MoveTankCommand moveTankCommand = new MoveTankCommand(ShouldDo.doit, new Angle(2.), new Speed(3.));
+        return moveTankCommand;
+
+
+        }
+
+
+
 
     @Override
-    public FireBulletCommand fireCommand(GameState gameState) {
-        throw new NotImplementedError();
-    }
+    public FireBulletCommand fireCommand(GameState gameState){
 
-}
+        final FireBulletCommand fireBulletCommand = new FireBulletCommand(ShouldDo.doit, new Angle(120.));
+        return fireBulletCommand;}}
+
+
